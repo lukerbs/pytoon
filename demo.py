966a9c72923598f7aa4c03b07d700d2d063da8b9
Luke Kerbs <LDK.kerbs@gmail.com>
2024-12-03 22:16:38 -0800

    updated demo

--- demo.py
+++ demo.py
@@ -2,29 +2,22 @@ from pytoon.animator import animate
 from pprint import pprint
 
 from moviepy.editor import (
-    VideoFileClip,
-    AudioFileClip,
-    vfx,
     ImageClip,
-    ImageSequenceClip,
-    concatenate_videoclips,
-    VideoClip,
-    CompositeVideoClip,
-    CompositeAudioClip,
 )
 
 FPS = 48
-text_path = "./.temp/speech.txt"
-audio_path = "./.temp/speech.mp3"
-output_video = "./test.mp4"
+text_path = "./.temp/speech.txt"  #  txt file with transcript text
+audio_path = "./.temp/speech.mp3"  #  audio file of speech
 
+# Load the transcript
 with open(text_path, "r") as file:
     transcript = file.read()
 
+# Generate the cartoon animation of the character saying the words in audio file
 animation = animate(audio_file=audio_path, transcript=transcript)
 
 video_clip = ImageClip("./.temp/image.png")
 video_clip.set_fps(FPS)
 video_clip.duration = animation.duration
 
-animation.export(path=output_video, background=video_clip)
+animation.export(path="./test.mp4", background=video_clip)
