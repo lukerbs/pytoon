8c50bd588fd7e67d647d18b67e51b298763618a1
Luke Kerbs <LDK.kerbs@gmail.com>
2024-12-03 22:17:56 -0800

    update files

--- demo.py
+++ demo.py
@@ -6,8 +6,8 @@ from moviepy.editor import (
 )
 
 FPS = 48
-text_path = "./.temp/speech.txt"  #  txt file with transcript text
-audio_path = "./.temp/speech.mp3"  #  audio file of speech
+text_path = "./.test/speech.txt"  #  txt file with transcript text
+audio_path = "./.test/speech.mp3"  #  audio file of speech
 
 # Load the transcript
 with open(text_path, "r") as file:
@@ -16,8 +16,8 @@ with open(text_path, "r") as file:
 # Generate the cartoon animation of the character saying the words in audio file
 animation = animate(audio_file=audio_path, transcript=transcript)
 
-video_clip = ImageClip("./.temp/image.png")
+video_clip = ImageClip("./.test/image.png")
 video_clip.set_fps(FPS)
 video_clip.duration = animation.duration
 
-animation.export(path="./test.mp4", background=video_clip)
+animation.export(path="./output_animation.mp4", background=video_clip)
